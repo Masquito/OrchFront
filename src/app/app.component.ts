f4e1e1d764fa8a7a5567af2c4c975da9c47a22d2
Aethillium <Aethillium@>
2024-04-15 19:54:15 +0200

    DZIAŁA AUTORYZACJA UŻYTTKOWNIKÓW Z BAZY ORAZ ROUTING

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { APIConnectionService } from '../../APIConnectionService/api-connection.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Orch_FrontAngu_Praca';
+
+  constructor(private API_COMM : APIConnectionService) {}
+
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,16 +1,65 @@
 import { Component } from '@angular/core';
+import { APIConnectionService } from '../../../APIConnectionService/api-connection.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { User } from '../../../Models/user';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  config!: User;
+  test: any;
+  myForm: FormGroup;
 
-  Login() : void {
-    console.log("BUTTON CLICKED")
+  constructor (private API_COMM : APIConnectionService, private router : Router, private fb : FormBuilder) {
+    this.myForm = this.fb.group({
+      email: [''],
+      password: ['']
+    });
   }
+
+  Login(){
+    this.GetApiDataFirstUser();
+  } 
+
+  ValidateUser(){
+    const email = this.myForm.get('email')?.value;
+    const pass = this.myForm.get('password')?.value;
+    if(email == this.config.Email && pass == this.config.Password){
+      this.router.navigate(['/Dashboard']);
+    }
+    else{
+      alert("zly username lub haslo");
+    }
+  }
+
+  GetApiDataFirstUser() {
+    this.API_COMM.getConfig()
+      .subscribe({
+        next: (data) => {
+          console.log('API Response:', data);
+          this.config = {
+            Id: data.Id,
+            Username: data.Username,
+            Password: data.Password,
+            Email: data.Email,
+            Region: data.Region,
+            Age: data.Age,
+            City: data.City,
+            ProfilePhoto: data.ProfilePhoto
+          };
+          this.ValidateUser();
+        },
+        error: (error) => {
+          console.error('API Error:', error);
+        }
+      });
+  }
+
 
 }
